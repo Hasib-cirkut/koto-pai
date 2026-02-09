67612535c9c1ce7a2d6d4a620e08d405e8a837da
Hasibul Huda <hasibulhuda1@gmail.com>
2026-02-10 00:10:37 +0600

    add pwa related changes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "channels/notifications_channel"
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker")
+  })
+}
