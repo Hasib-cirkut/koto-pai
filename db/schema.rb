80e136bbc9d8d845d85de90337bb1ea4e4d008ec
Hasibul Huda <hasibulhuda1@gmail.com>
2026-02-10 01:54:03 +0600

    add solid cable to primary db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_07_005950) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_09_195057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -51,6 +51,16 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_07_005950) do
     t.datetime "updated_at", null: false
     t.index ["chain_id"], name: "index_checkpoints_on_chain_id"
     t.index ["created_by_id"], name: "index_checkpoints_on_created_by_id"
+  end
+
+  create_table "solid_cable_messages", force: :cascade do |t|
+    t.binary "channel", null: false
+    t.binary "payload", null: false
+    t.datetime "created_at", null: false
+    t.bigint "channel_hash", null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
+    t.index ["channel_hash"], name: "index_solid_cable_messages_on_channel_hash"
+    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
   end
 
   create_table "users", force: :cascade do |t|
